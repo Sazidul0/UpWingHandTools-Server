acec722265169cddcfe01a1e32e568da034bc141
rejaul-islam1 <rejaul.hero@gmail.com>
2022-05-23 22:51:30 +0600

    Verify admin added

--- index.js
+++ index.js
@@ -94,8 +94,20 @@ async function run() {
             res.send(users);
         })
 
+        // Verify Admin
+        const verifyAdmin = async (req, res, next) => {
+            const requester = req.decoded.email;
+            const requesterAccount = await userCollection.findOne({ email: requester })
+            if (requesterAccount.role === 'admin') {
+                next();
+            }
+            else {
+                res.status(403).send({ message: 'Forbidden' })
+            }
+        }
+
         // Create or Upadate Admin
-        app.put('/user/admin/:email', verifyJWT, async (req, res) => {
+        app.put('/user/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
             const email = req.params.email;
             const filter = { email: email };
             const updatedDoc = {
@@ -103,6 +115,14 @@ async function run() {
             };
             const result = await userCollection.updateOne(filter, updatedDoc)
             res.send(result);
+        })
+
+        // Get Admin
+        app.get('/admin/:email', verifyJWT, verifyAdmin, async (req, res) => {
+            const email = req.params.email;
+            const user = await userCollection.findOne({ email: email });
+            const isAdmin = user.role === 'admin';
+            res.send({ admin: isAdmin });
         })
     }
     finally {
