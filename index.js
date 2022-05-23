52f90e1b84a95eb321492b6cd47d9bd094c5b26f
rejaul-islam1 <rejaul.hero@gmail.com>
2022-05-23 13:11:09 +0600

    Tools collection added

--- index.js
+++ index.js
@@ -1,11 +1,45 @@
 const express = require('express');
 const cors = require('cors');
 require('dotenv').config();
+const { MongoClient, ServerApiVersion, ObjectId } = require('mongodb');
 const app = express();
 const port = process.env.PORT || 5000;
 
 app.use(cors());
 app.use(express.json())
+
+const uri = `mongodb+srv://${process.env.DB_USER}:${process.env.DB_PASS}@cluster0.mg6pk.mongodb.net/?retryWrites=true&w=majority`;
+
+const client = new MongoClient(uri, { useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 });
+
+
+async function run() {
+    try {
+        await client.connect();
+        const toolsCollection = client.db('upwing-hand-tools').collection('tools');
+
+        // Getting all Tools
+        app.get('/tools', async (req, res) => {
+            const query = {};
+            const cursor = toolsCollection.find(query)
+            const result = await cursor.toArray();
+            res.send(result);
+        })
+
+        // Get one Tool
+        app.get('/tools/:id', async (req, res) => {
+            const id = req.params.id;
+            const query = { _id: ObjectId(id) };
+            const result = await toolsCollection.findOne(query);
+            res.send(result);
+        })
+    }
+    finally {
+
+    }
+}
+run().catch(console.dir);
+
 
 app.get('/', (req, res) => {
     res.send("Upwing server is running");
