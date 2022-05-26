268db300c0dd20de86f089b1d5161375f3988037
rejaul-islam1 <rejaul.hero@gmail.com>
2022-05-26 11:03:35 +0600

    Final commit

--- index.js
+++ index.js
@@ -7,6 +7,7 @@ const app = express();
 const port = process.env.PORT || 5000;
 const stripe = require('stripe')(process.env.STRIPE_SECRET_KEY);
 
+// Middleware
 app.use(cors());
 app.use(express.json())
 
@@ -165,8 +166,8 @@ async function run() {
             res.send(result);
         })
 
-        // Delete a Order by Admin
-        app.delete('/allorder/:id', verifyJWT, verifyAdmin, async (req, res) => {
+        // Delete a Order by Admin or a User 
+        app.delete('/allorder/:id', verifyJWT, async (req, res) => {
             const id = req.params.id;
             const filter = { _id: ObjectId(id) };
             const result = await orderCollection.deleteOne(filter);
